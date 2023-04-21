9a890e0750b9d6b86094e166945e9f0c9882131b
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-04-21 20:20:32 +0100

    testfile

---
+++ src/ParkingLotApp/Models/DataFromFile.cs
@@ -0,0 +1,11 @@
+namespace ParkingLotApp.Models
+{
+    public class DataFromFile
+    {
+        public string? ParkingLot { get; set; }
+        public string? Vehicle { get; set; }
+        public string? VehicleParkTime { get; set; }
+        public string? VehicleUnparkTime { get; set; }
+        public string? Action { get; set; }
+    }
+}
--- src/ParkingLotApp/Program.cs
+++ src/ParkingLotApp/Program.cs
@@ -15,14 +15,69 @@ public class Program
             .AddSingleton<IParkingReceipt, ParkingLotApp.Services.ParkingReceipt>()
             .BuildServiceProvider();
 
-
         var vehicleParking = serviceProvider.GetRequiredService<IVehicleParking>();
-        //var parkingLot = serviceProvider.GetRequiredService<IParkingLot>();
-        //string jsonString = JsonSerializer.Serialize<ParkingTicket>(parkingLot.AirportParking("Cars_SUV"));
+        var parkingLot = serviceProvider.GetRequiredService<IParkingLot>();
         var parkingReceipt = serviceProvider.GetRequiredService<IParkingReceipt>();
-        string jsonString = JsonSerializer.Serialize<ParkingLotApp.Models.ParkingReceipt>(parkingReceipt.AirportParking("2016-12-01T00:00:00", "2016-12-31T23:59:59", "Cars_SUV"));
+        try
+        {
+            DataFromFile dataFromFile = new DataFromFile();
+            var jsonString = File.ReadAllText("TestFile.json");
+            using (JsonDocument jsonDocument = JsonDocument.Parse(jsonString))
+            {
+                JsonElement root = jsonDocument.RootElement;
+                dataFromFile.ParkingLot = root.GetProperty("ParkingLot").ToString();
+                dataFromFile.Vehicle = root.GetProperty("Vehicle").ToString();
+                dataFromFile.VehicleParkTime = root.GetProperty("ParkTime").ToString();
+                dataFromFile.VehicleUnparkTime = root.GetProperty("UnparkTime").ToString();
+                dataFromFile.Action = root.GetProperty("Action").ToString();
+            }
+            if (dataFromFile.Action == "Park")
+            {
+                switch (dataFromFile.ParkingLot.ToString())
+                {
+                    case "Airport":
+                        Console.WriteLine(JsonSerializer.Serialize<ParkingTicket>(parkingLot.AirportParking(dataFromFile.Vehicle)));
+                        break;
+                    case "Stadium":
+                        Console.WriteLine(JsonSerializer.Serialize<ParkingTicket>(parkingLot.StadiumParking(dataFromFile.Vehicle)));
+                        break;
+                    case "Mall":
+                        Console.WriteLine(JsonSerializer.Serialize<ParkingTicket>(parkingLot.MallParking(dataFromFile.Vehicle)));
+                        break;
+                    default:
+                        Console.WriteLine("Not able to park your vehicle now");
+                        break;
+                }
+            }
 
-        Console.WriteLine(jsonString);
+            if (dataFromFile.Action == "Unpark")
+            {
+                switch (dataFromFile.ParkingLot.ToString())
+                {
+                    case "Airport":
+                        Console.WriteLine(JsonSerializer.Serialize<ParkingLotApp.Models.ParkingReceipt>(parkingReceipt.AirportParking(dataFromFile.VehicleParkTime, dataFromFile.VehicleUnparkTime, dataFromFile.Vehicle)));
+                        break;
+                    case "Stadium":
+                        Console.WriteLine(JsonSerializer.Serialize<ParkingLotApp.Models.ParkingReceipt>(parkingReceipt.StadiumParking(dataFromFile.VehicleParkTime, dataFromFile.VehicleUnparkTime, dataFromFile.Vehicle)));
+                        break;
+                    case "Mall":
+                        Console.WriteLine(JsonSerializer.Serialize<ParkingLotApp.Models.ParkingReceipt>(parkingReceipt.MallParking(dataFromFile.VehicleParkTime, dataFromFile.VehicleUnparkTime, dataFromFile.Vehicle)));
+                        break;
+                    default:
+                        Console.WriteLine("Not able to unpark your vehicle now");
+                        break;
+                }
+            }
+
+            //string jsonString = JsonSerializer.Serialize<ParkingTicket>(parkingLot.AirportParking("Cars_SUV"));
+
+            //string jsonString = JsonSerializer.Serialize<ParkingLotApp.Models.ParkingReceipt>(parkingReceipt.AirportParking("2016-12-01T00:00:00", "2016-12-31T23:59:59", "Cars_SUV"));
+            //Console.WriteLine(jsonString);
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
     }
 
 }
